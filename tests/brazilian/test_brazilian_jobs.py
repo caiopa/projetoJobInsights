222ee0e96eea3df709cb86b5b62d3cd21d61dde7
Caio Pereira Antunes <caioefg@gmail.com>
2023-01-24 13:50:17 -0300

    test: criando teste para o requisito 12

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+mock = {"title": "", "salary": "", "type": ""}
+path = 'tests/mocks/brazilians_jobs.csv'
 
 
 def test_brazilian_jobs():
     pass
+    read_file = read_brazilian_file(path)
+
+    for job in read_file:
+        assert mock.keys() == job.keys()
