644ab6c03592e1325c77ad3d799acdf0d6086de4
Caio Pereira Antunes <caioefg@gmail.com>
2023-01-24 14:13:02 -0300

    test: test func sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,28 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+mocks = [
+
+    {"min_salary": 500, "max_salary": 1500},
+    {"min_salary": 1000, "max_salary": 2500},
+    {"min_salary": 1500, "max_salary": 3500},
+]
+
+order_max = [ 
+    {"min_salary": 1500, "max_salary": 3500},
+    {"min_salary": 1000, "max_salary": 2500},
+    {"min_salary": 500, "max_salary": 1500},
+]
+
+order_min = [
+    {"min_salary": 500, "max_salary": 1500},
+    {"min_salary": 1000, "max_salary": 2500},
+    {"min_salary": 1500, "max_salary": 3500},
+]
 
 
 def test_sort_by_criteria():
+    sort_by(mocks, "min_salary")
+    assert mocks == order_min
+    sort_by(mocks, "max_salary")
+    assert mocks == order_max
     pass
